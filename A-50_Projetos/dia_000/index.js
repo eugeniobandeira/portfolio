4aeb87a6be596dec944b6c21b69103e139036c17
eugeniobandeira <eugeniobandeira00@gmail.com>
2022-11-29 19:19:39 -0300

    adicionando a calculadora de gorjeta

---
+++ A-50_Projetos/dia_000/index.js
@@ -0,0 +1,17 @@
+
+function update() {
+    let bill  = Number(document.getElementById('yourBill').value)
+    let tipPercent = document.getElementById('tipInput').value
+    let split = document.getElementById('splitInput').value
+    
+    let tipValue = bill * (tipPercent / 100)
+    let billTotal = bill + tipValue
+    let billEach = billTotal / split
+    
+    document.getElementById('tipPercent').innerHTML = tipPercent + '%'
+    document.getElementById('tipValue').innerHTML = 'R$ ' + tipValue.toFixed(2)
+    document.getElementById('totalWithTip').innerHTML = 'R$ ' + billTotal.toFixed(2)
+    document.getElementById('splitValue').innerHTML = split
+    document.getElementById('billEach').innerHTML = Number(billTotal / split).toFixed(2)
+
+}
